aa0b40e3180bcf092304203acf089728ef700327
Dylan-Oleary <dylaneamusic@gmail.com>
2019-08-05 14:56:41 -0400

    Initial Commit

---
+++ Fast-Tap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fast-Tap
+//
+//  Created by Dylan O'Leary on 2019-08-05.
+//  Copyright © 2019 Dylan O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
